b712de1196bde0c2bc64796840ec909f47a411a7
OxPAN <kipir1@mail.ru>
2021-10-29 21:18:43 +0700

    Портированы delete, findNode, find, и т.д. произведены тесты testDrive Заметка: в find лежит послание

--- src/main/scala/List/TList.scala
+++ src/main/scala/List/TList.scala
@@ -1,7 +1,7 @@
 package git.group
 package List
 
-class TList[T]{
+class TList[T](limit:Int){
   class Node[T](data1:T){
     var data:T = data1
     var next:Node[T] = null
@@ -10,7 +10,7 @@ class TList[T]{
   private var head:Node[T] = null
   private var tail:Node[T] = null
   private var size:Int = 0
-  private var size_limit:Int = 200
+  private var size_limit:Int = limit
 
   def pushFront(data:T):Boolean = {
     if (size < size_limit){
@@ -78,6 +78,87 @@ class TList[T]{
     false
   }
 
+  def delete(index:Int):Boolean = {
+    if (index < 0) {
+      return false
+    }
+    var toDel: Node[T] = null
+    var toDelPrev: Node[T] = null
+
+    if (head == null) {
+      println("List is empty")
+      return false
+    }
+    else {
+      if (head != tail) {
+        if (index > 0) {
+          toDelPrev = findNode(index - 1)
+          toDel = toDelPrev.next
+        }
+        else toDel = head
+
+        if (toDelPrev != null) {
+          toDelPrev.next = toDel.next
+          toDel = null
+        }
+        else {
+          head = toDel.next
+          toDel = null
+        }
+      }
+      else {
+        head = null
+        tail = null
+      }
+
+    }
+    size = size - 1
+    true
+  }
+
+  private def findNode(id:Int):Node[T] = {
+    var res:Node[T] = head
+    var n:Int = 0
+    while (n < id){
+      res = res.next
+      n = n + 1
+    }
+    res
+  }
+
+  def find(index:Int):T = {
+    var current:Node[T] = head
+    var dataNode:T = current.data  /// Назойлевый коостыль найти по этой хрени инфу. Не забудь!
+    if (index == 0){
+      dataNode = current.data
+      return dataNode
+    }
+    var n:Int = 0
+    while (n < index){
+      current = current.next
+      n = n + 1
+    }
+    dataNode = current.data
+    dataNode
+  }
+
+  def finds(obj:T):Int = {
+    var current:Node[T] = head
+    var index:Int = 0
+    if (head == null){
+      return -1
+    }
+    else {
+      while (current != null){
+        if (current.data == obj){
+          return index
+        }
+        index = index + 1
+        current = current.next
+      }
+    }
+    -1
+  }
 
   //временный метод служащий для тестов списка на начальных этапах
   def print():Boolean = {
@@ -104,9 +185,17 @@ class TList[T]{
 
 
 //GET
-//SET
+//SET       пока я в запутоности от скалы нужныли эти сетеры или нет но пускай пока лежат
   def getSize():Int = {
     size
+  }
+  def getSizeLimit:Int = size_limit
+  def setSizeLimit(limit:Int):Boolean = {
+    if(limit <= 0 || limit <= size){
+      return false
+    }
+    size_limit = limit
+    true
   }
 
 }
--- src/main/scala/View/ConsolApp.scala
+++ src/main/scala/View/ConsolApp.scala
@@ -4,7 +4,7 @@ package View
 import List.TList
 
 class ConsolApp {
-  var list:TList[Int] = new TList()
+  var list:TList[Int] = new TList(10)
 
 
   def testCode() = {
@@ -17,15 +17,51 @@ class ConsolApp {
     list.add(40,2)
     list.add(1,1)
     println(list.getSize())
+    println(list.getSizeLimit)
     println("list: ")
 
     list.print()
+    println()
+    println("delete list: ")
+    list.delete(2)
+    list.print()
+    println()
+    println("find list: ")
+    println("[2] "+list.find(2))
+    println("[int 30] "+list.finds(30))
+
+
+  }
+
+  def testDrive(maxElement:Int) = {
+    var list:TList[Int] = new TList(maxElement)
+    var n:Int = 0;
+    var i:Int = 0;
+    while (n < maxElement){
+      i = scala.util.Random.nextInt(maxElement).toInt
+      list.pushEnd(i)
+      n = n + 1
+    }
+    list.print()
+    println()
+    println("size: "+list.getSize())
+
+
+    n = 0
+    while (n < maxElement){
+      list.delete(0)
+      n = n + 1
+    }
+    println("clear")
+
 
 
   }
 
   def run() = {
-    testCode()
+//    testCode()
+    testDrive(40)
   }
+
 
 }
